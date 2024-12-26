1f780e228af39dc1d83dafe7bf6c3e30624a462c
Mambetkulov <muganm320@gmail.com>
2024-12-26 16:15:40 +0600

    muha

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,9 @@ public class Main {
 
 
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
 
 
 

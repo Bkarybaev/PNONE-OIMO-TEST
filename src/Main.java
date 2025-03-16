9e96a8751e0fa6cd708d76c72002b65d16c02630
Bkarybaev <baielkarybaev@gmail.com>
2025-03-16 07:22:41 +0600

    add funtion crud

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,12 @@ public class Main {
     }
     public Main(){
         windows = new JFrame();
-
-
         windows.setSize(500,750);
         System.out.println("hello world");
         windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windows.getContentPane().setBackground(Color.darkGray);
         windows.setLocationRelativeTo(null);
+
         windows.setLayout(null);
         panel = new JPanel();
         panel.setBounds(0,0,580,990);
